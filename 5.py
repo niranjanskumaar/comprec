de0fd3f8183a6183ad440e1b09961122446ab962
Niranjan S Kumar <niranjanskumar@outlook.com>
2025-01-17 20:07:44 +0530

    Add  hint 5.py

--- 5.py
+++ 5.py
@@ -12,7 +12,7 @@ def swap(l):
 
     if n % 2 == 0: # if there are even no of elements,
         for i in range(0, n, 2):
-            l[i], l[i+1] = l[i+1], l[i] # swap every two elements
+            l[i], l[i+1] = l[i+1], l[i] # swap every two elements (a, b, c, d) => (b, a, d, c)
 
     else:
         for i in range(0, n-1, 2):  # if there are odd no of elements
