b9916545d19d4403a6ea654f4615f99269e8b1bd
Niranjan S Kumar <niranjanskumar@outlook.com>
2025-01-17 19:37:13 +0530

    Fix 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,14 @@
+#4 
 
+"""
+Def a func that takes an int as argument.
+Returns 0 if num is prime, 1 if not prime.
+Write program to copy numbers of a list to a new list
+"""
 
 def prime(x):
     p = 1
+    
     if x != 1:
         for j  in range(2, x): # j can be any number from 2 to x
             if x % j == 0: # if x can be divided by j
@@ -9,13 +16,13 @@ def prime(x):
     
     return p
 
-n = int(input("n: "))
+n = int(input("Enter no of elements: : "))
 
 l = []
 pl = [] # prime nos list
 
 for i in range(n):
-    no = int(input("No: "))
+    no = int(input("Enter number: "))
     l.append(no)
 
 # now, aa listile prime nos ellam koodi 'pl' listilott idam
