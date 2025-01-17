80a70586ff432d11519bee880ecdb62db65c2269
Niranjan S Kumar <niranjanskumar@outlook.com>
2025-01-17 20:25:49 +0530

    11.py

--- 11.py
+++ 11.py
@@ -1,22 +1,35 @@
-# create binary file to store details of n employees in a binary file and display it on the screen
+
+"""
+11. Create binary file to store details of n employees in a 
+    binary file and display it on the screen
+"""
 
 import pickle
 
+# creating the file
+
 def create():
     wf = open("./files/employ.dat", "wb")
-    n = int(input("n: "))
+    n = int(input("Enter no of employees: "))
+    
     for i in range(n):
         eno = int(input("Emp no: "))
         ename = input("Emp name: ")
         salary = int(input("Salary: "))
 
         l = [eno, ename, salary]
-        pickle.dump(l)
+    
+        pickle.dump(l, wf)
 
     wf.close()
 
+
+# displaying the file
+
 def display():
     rf = open("./files/employ.dat", "rb")
+
+    print("Displaying the file..\n")
     try:
         while True:
             l = pickle.load(rf) # load the file line by line
